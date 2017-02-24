ac9e14153f1923e1321f7749b97d31ee7d5fc730
hk.kim <savekhg@gmail.com>
2017-02-24 13:41:36 +0000

    python env

--- colorpick/settings.py
+++ colorpick/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -119,8 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#STATIC_URL = os.path.join(BASE_DIR,"/static/")
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]                                             #추가
+#STATICFILES_DIRS = (
+#    PROJECT_PATH+'/../static',
+#)
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR,"static"),
+  '/home/ec2-user/bebep/static/',
 )
