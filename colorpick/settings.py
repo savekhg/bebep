454ba0ff8fdacb22b2474746247383a10541b172
hk.kim <savekhg@gmail.com>
2017-03-04 21:50:43 +0900

    setting.py update for brand url

--- colorpick/settings.py
+++ colorpick/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'colorpick.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, '../brand/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-TEMPLATE_DIRS = (os.path.join(BASE_DIR),)
 
 WSGI_APPLICATION = 'colorpick.wsgi.application'
 
