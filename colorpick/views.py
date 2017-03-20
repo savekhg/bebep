0ad7198f0327e1edc0eaaa29d88df1ea46bde9d9
hk.kim <savekhg@gmail.com>
2017-03-20 23:12:38 +0900

    db<->django 연동, s3링크 test, brand부분 개선필요함

--- colorpick/views.py
+++ colorpick/views.py
@@ -1,12 +1,13 @@
 from django.views.generic.base import TemplateView
 
 # Create your views here.
-
+from web.models import CommonCodeTb
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        context['brandCommonCode'] = CommonCodeTb.objects.filter(upper_common_cd='00', user='1').order_by('order')
 
         return context
